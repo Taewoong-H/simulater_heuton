f4375e35920dec171a563e39ba2aaff8faca48e4
Taewoong-H <taeung2008@gmail.com>
2024-01-15 01:10:59 +0900

    feat: 메인 화면 세팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Scenario from './Scenario';
+import MainPage from './pages/MainPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <body className="App-body">Hello World!</body>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/scenario" element={<Scenario />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
---
+++ src/pages/MainPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div className="main-container">
+      <h1 className="title">내가 쓰는 내 이야기</h1>
+      <p className="quotation">
+        “우리가 조금 더 의식적으로 우리 자신의 저작권 (authorship)을 갖지 않으면 남들이 대신 우리 이야기를 쓰게 될
+        것이다.”
+      </p>
+      <p className="quotation-source">
+        데런 브라운, {'<'}모든 것이 괜찮아지는 기술{'>'}
+      </p>
+      <div>
+        <img className="main-image" alt="작성하는 사람" src="img/main_image.png" />
+      </div>
+      <div className="input-container">
+        <input></input>
+      </div>
+      <div className="button-container">
+        <Link to="/scenario" className="start-button" style={{ textDecoration: 'none' }}>
+          시작하기
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
